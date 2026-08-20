be43ea632520d1fccb3543b3cf845605964da909
arunz6 <arunrajput88176894@gmail.com>
2026-08-20 17:02:33 +0530

    /

--- Main.c
+++ Main.c
@@ -124,3 +124,28 @@ int main()
 //     printf("now a is %d\n", a);
 //     printf("b value is %d", b);
 // }
+
+// swap 3 var without temp
+
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     // swap a number with 3rd value
+//     int a, b,c;
+//     printf("give the value 1st value");
+//     scanf("%d", &a);
+//     printf("give the value 2st value");
+//     scanf("%d", &b);
+//      printf("give the value 3st value");
+//     scanf("%d", &c);
+//  a = a + b + c;
+//     c = a - b - c;
+//     b = a - b - c;
+//     a = a - b - c;
+//     printf("now a is %d\n", a);
+//     printf("b value is %d\n", b);
+//       printf("c value is %d", c);
+
+// }
