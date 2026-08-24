7baa790df7135ef2757625144e7659c43ee7f365
arunz6 <arunrajput88176894@gmail.com>
2026-08-24 22:32:30 +0530

    Add initial implementation of a C program for basic arithmetic operations

---
+++ cAssing.c
@@ -0,0 +1,18 @@
+// Write a C program to input two numbers and print their sum, difference, product, quotient, andremainder.
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     int a;
+//     int b;
+//     printf("Enter first number: ");
+//     scanf("%d", &a);
+//     printf("Enter second number: ");
+//     scanf("%d", &b);
+//     printf("Sum : %d\n", a + b);
+//     printf("Difference = %d\n", a - b);
+//     printf("Product : %d\n", a * b);
+//     printf("Quotient = %d\n", a / b);
+//     printf("Remainder = %d\n", a % b);
+// }
