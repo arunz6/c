9f2a26b9ce0b205c1cc4e9a373cfda1d7c00a424
arunz6 <arunrajput88176894@gmail.com>
2026-08-27 00:20:49 +0530

    Add commented code examples for assignment and conditional operators in cAssing.c

--- cAssing.c
+++ cAssing.c
@@ -20,22 +20,17 @@
 // Write a program to calculate the area and perimeter of a rectangle.
 
 // #include <stdio.h>
-
 // void main()
 // {
 //     int l, b;
 //     int area;
 //     int perimeter;
-
 //     printf("Enter length: ");
 //     scanf("%d", &l);
-
 //     printf("Enter breadth: ");
 //     scanf("%d", &b);
-
 //     area = l * b;
 //     perimeter = 2 * (l + b);
-
 //     printf("Area = %.2d\n", area);
 //     printf("Perimeter = %.2d\n", perimeter);
 // }
@@ -46,20 +41,14 @@
 // int main()
 // {
 //     int p, r, t, smpintrst;
-
 //     printf("Enter principal amount: ");
 //     scanf("%d", &p);
-
 //     printf("Enter rate    of interest: ");
 //     scanf("%d", &r);
-
 //     printf("Enter time in years: ");
 //     scanf("%d", &t);
-
 //     smpintrst = (p * r * t) / 100;
-
 //     printf("Simple Interest = %d\n", smpintrst);
-
 //     return 0;
 // }
 
@@ -67,64 +56,58 @@
 // question he samajh nai aya .
 
 // Write a program to convert seconds into hours, minutes, and seconds.
+
 // #include <stdio.h>
+
 // int main()
 // {
 //     int ts;
 //     int h, m, sec;
-
 //     printf("Enter total seconds: ");
 //     scanf("%d", &ts);
-
 //     h = ts / 3600;
 //     ts = ts % 3600;
-
 //     m = ts / 60;
 //     sec = ts % 60;
-
 //     printf("Hours = %d\n", h);
 //     printf("Minutes = %d\n", m);
 //     printf("Seconds = %d\n", sec);
-
 //     return 0;
 // }
 
 // 2. Relational Operators &lt; &gt; &lt;= &gt;= == !=
 
 // Write a program to input two numbers and check which number is greater.
+
 // #include <stdio.h>
+
 // int main()
 // {
 //     int a,b;
-
 //     printf("Enter first number: ");
 //     scanf("%d",&a);
-
 //     printf("Enter second number: ");
 //     scanf("%d", &b);
-
 //     if(a>b)
 //         printf("  greater %d",a);
 //     else
 //         printf("  greater %d", b);
-
 //     return 0;
 // }
 
 // // Write a program to check whether a number is equal to zero, positive, or negative.
+
 // #include <stdio.h>
+
 // int main()
 // {
 //     int n;
-
 //     printf("Enter a number: ");
 //     scanf("%d", &n);
-
 //     if (n >= 0)
 //         printf("Number is positive");
 //     else
 //         printf("Number is negative");
-
 //     return 0;
 // }
 
@@ -134,37 +117,30 @@
 // int main()
 // {
 //     int marks;
-
 //     printf("Enter marks: ");
 //     scanf("%d",&marks);
-
 //     if(marks>=33)
 //         printf("Passed\n");
 //     else
 //         printf("Failed\n");
-
 //     return 0;
 // }
 
 // // Write a program to find the largest of two numbers using relational operators.
+
 // #include <stdio.h>
 // int main()
 // {
 //     int x,y,large;
-
 //     printf("Enter first number: ");
 //     scanf("%d", &x);
-
 //     printf("Enter second number: ");
 //     scanf("%d",&y);
-
 //     if(x>=y)
 //         large = x;
 //     else
 //         large=y;
-
 //     printf("Largest number = %d\n", large);
-
 //     return 0;
 // }
 
@@ -262,3 +238,108 @@
 // }
 
 // logical oprator done
+
+// 4. Assignment Operators = += -= *= /= %=
+
+// Write a program to demonstrate all compound assignment operators.
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     printf("give the value");
+//     scanf("%d", &a);
+//     a += 5;
+//     printf("a += 5  : %d\n", a);
+
+//     a -= 3;
+//     printf("a -= 3  : %d\n", a);
+
+//     a *= 2;
+//     printf("a *= 2  : %d\n", a);
+
+//     a /= 4;
+//     printf("a /= 4  : %d\n", a);
+
+//     a %= 3;
+//     printf("a %%= 3  : %d\n", a);
+//     return 0;
+// }
+
+// Write a program that increases a number by 10 using +=.
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     printf("give the value");
+//     scanf("%d", &a);
+//     a += 10;
+//     printf("value is incresed by 10 using += %d", a);
+//     return 0;
+// }
+
+// Write a program that decreases a number by 5 using -=.
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     printf("give the value");
+//     scanf("%d", &a);
+//     a -= 5;
+//     printf("value is incresed by 10 using += %d", a);
+//     return 0;
+// }
+
+// Write a program to calculate the final value of a variable after applying +=, -=, *=, and /=.
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     printf("give the value");
+//     scanf("%d", &a);
+//     a += 10;
+//     a -= 5;
+//     a *= 2;
+//     a /= 5;
+//     printf("Final value of a = %d", a);
+//     return 0;
+// }
+
+// 5. Bitwise Operators &amp; | ^ ~ &lt;&lt; &gt;&gt;
+
+// ye bad me karunga
+
+// 6. Conditional Operator ?:
+
+// Write a program to find the largest of two numbers using the conditional operator.
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     printf("give the value");
+//     scanf("%d", &a);
+//     int b;
+//     printf("give the value");
+//     scanf("%d", &b);
+//     a > b ? printf("a is bigger ") : printf("b is bigger ");
+//     return 0;
+// }
+
+// Write a program to check whether a number is even or odd using ?:.
+
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    printf("give the value");
+    scanf("%d", &a);
+
+    a % 2 == 0 ? printf("a is even ") : printf("a is odd");
+}
